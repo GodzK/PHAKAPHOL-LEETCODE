245c177d6f9c2ba498e14f06ac4a3b35dec234b9
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-03-19 09:45:12 +0700

    🥪Adding a regex notes

--- Javascript/MasterKK/StringManipulate/Regex/c07_regex.js
+++ Javascript/MasterKK/StringManipulate/Regex/c07_regex.js
@@ -1,16 +1,21 @@
-// Regular Expression
+// Regular Expression สำหรับการ Process Text
 
-function create(){
-    let t = "this is String from checking regular expression"
-    let r = /expression/i //มันมีinมั้ย ไม่สนใจตัวใหญ่ตัวเล็ก
-    console.log("text : " , t);
-    console.log("regex.source: " , r.source);
-    console.log("regex.flags" , r.flags);
-    console.log(`Type of Regex r : `, typeof(r));
-    console.log(`test : `, r.test(t));
-
+function create() {
+  let t = "Hello World";
+  let pattern = /World/gi
+  /* การใช้ /pattern/flags (Literal Notation) */
+  /* Flag  
+    g	ค้นหาทุกที่ (global search)
+    i	ไม่สนตัวพิมพ์เล็ก-ใหญ่ (case insensitive)
+    m	ค้นหาหลายบรรทัด (multi-line)
+    s	ทำให้ . ตรงกับ newline (\n) ได้
+    u	รองรับ Unicode
+    y	ค้นหาแบบ sticky (เริ่มจากตำแหน่งปัจจุบัน)*/
+    console.log("-----START FUNCTION------");
+  console.log("Find Matching Word : ",t.match(/World/g));//word/condition
+  //test ว่ามี "check" ใน regexมั้ย
+  console.log(".test() regex : ",pattern.test("World"));//true
+  let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 }
 
-create()
-
-
+create();
